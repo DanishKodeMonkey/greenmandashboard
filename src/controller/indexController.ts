348772407105186af962e83f928c6b2e811d054e
danishkodemonkey <humberjini@hotmail.com>
2024-09-23 15:26:43 +0200

    handle error cases

--- src/controller/indexController.ts
+++ src/controller/indexController.ts
@@ -11,8 +11,12 @@ res: Response(object type shape)<DashboardResponse>(expected data shape)
 : void is the expected return type, it is normally inferred in this case (as express returns a res object) but set explicitly for example sake
 */
 const getDashboard = asyncHandler(async(req: Request, res: Response<DashboardResponse>)=>{
+    try{
     const posts = await postQueries.getAllPosts()
-    res.render('index', {title: 'Dashboard', posts})
+    res.render('index', {title: 'Dashboard', posts: posts, errors: []})
+}catch(err){
+    res.render('index', {title: 'Dashboard', posts: [], errors: err})
+}
 })
 
 // export for router
